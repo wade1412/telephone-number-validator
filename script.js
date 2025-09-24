f445a177ed75d53a7286d94cdbe02c7672e53ecb
Valeriy Petrenko <fourcher00@gmail.com>
2025-09-24 14:58:48 -0400

    Debuggin, refactoring, final function added to button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const clearBtn = document.getElementById("clear-btn");
 const result = document.getElementById("results-div");
 
 const isInputEmpty = (str) => {
-  return str.trim() === "" ? true : false;
+  return str.trim() === "";
 };
 
 const isPhoneNumberLengthValid = (arr) => {
@@ -16,27 +16,38 @@ const isPhoneNumberLengthValid = (arr) => {
 };
 
 const areBracketsValid = (arr) => {
-  if (
+  return (
     arr.indexOf("(") - arr.indexOf(")") === -4 ||
     arr.indexOf("(") - arr.indexOf(")") === 0
-  )
-    return true;
-  else return false;
+  );
 };
 
 const inputCheck = (str) => {
   const invalidCharactersRegex = /[a-zA-Z!@#$%^&*_+\=\[\]{};':"\\|,.<>\/?~]/;
-  const bracketsRegex = /[-()]/;
-  let cleanStr = str.replace(bracketsRegex, "").trim();
+  const bracketsRegex = /[-()\s]+/gi;
+  const cleanStr = str.replace(bracketsRegex, "").trim().split("");
+
   if (
     invalidCharactersRegex.test(str) ||
     !str.startsWith("1") ||
-    !isPhoneNumberLengthValid(str) ||
+    !isPhoneNumberLengthValid(cleanStr) ||
     !areBracketsValid(str)
   ) {
     return false;
   } else return true;
 };
+
+const phoneNumberCheck = (str) => {
+  if (isInputEmpty(str))
+    return (result.innerText = `Please provide a phone number`);
+  else if (inputCheck(str))
+    return (result.innerText = `Valid US number: ${str}`);
+  else return (result.innerText = `Invalid US number: ${str}`);
+};
+
+checkBtn.addEventListener("click", () => {
+  phoneNumberCheck(userInput.value);
+});
 
 clearBtn.addEventListener("click", () => {
   userInput.value = "";
